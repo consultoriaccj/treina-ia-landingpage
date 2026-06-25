69e9ba5e4d6b5e82afe98f945682be9ba2d27d0f
consultoriaccj <contato@consultoriaccj.com.br>
2026-06-25 10:00:20 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1052,7 +1052,7 @@ export default function App() {
               {/* Partner 1: Sebrae */}
               <div className="flex flex-col items-center gap-1 border border-slate-900 p-3 rounded-lg bg-slate-900/10 w-full">
                 <img 
-                  src="https://raw.githubusercontent.com/consultoriaccj/treina-ia-landingpage/main/sebrae.png" 
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Sebrae_logo.svg/2560px-Sebrae_logo.svg.png" 
                   alt="Sebrae" 
                   className="h-8 object-contain filter grayscale" 
                   referrerPolicy="no-referrer"
