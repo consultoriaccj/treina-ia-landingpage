3b0f433316948ad57db026c35fd54db8e84dd0e0
consultoriaccj <contato@consultoriaccj.com.br>
2026-06-25 09:00:34 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -160,7 +160,7 @@ export default function App() {
               <div className="absolute inset-0 bg-blue-500 rounded-lg blur-sm opacity-50 group-hover:opacity-100 transition-opacity"></div>
               {/* Image Placeholder for Logo - Customizable by user */}
               <img 
-                src="src="/logotreinaia.png" 
+                src="/logotreinaia.png" 
                 alt="Treina IA Logo" 
                 className="relative h-9 w-9 rounded-lg object-cover border border-blue-500/50" 
                 referrerPolicy="no-referrer"
